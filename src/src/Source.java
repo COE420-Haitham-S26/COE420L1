549f6e449506cd41b42d282466482186de903b5d
HaithamAyyash <haithamayyash5@gmail.com>
2026-01-22 21:20:05 +0400

    Feature B added

--- src/src/Source.java
+++ src/src/Source.java
@@ -1,8 +1,18 @@
 package src;
 
 public class Source {
+	 public void featureB() {
+		System.out.println("Feature B was called");
+	}
+
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		featureB();
+		
+
 	}
+	public static void featureB() {
+	
+
 
 }
